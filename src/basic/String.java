c874325029205d8767113fb037cee121fd5b5cd6
Nivedita Gavade <nivedita.gavade19@gmail.com>
2023-05-23 09:00:20 +0530

    check change

--- src/basic/String.java
+++ src/basic/String.java
@@ -3,8 +3,8 @@ package basic;
 public class String {
 	public static void main(String[] args) {
 		System.out.println("snnnnnnnnnn");;
-		System.out.println("stash5");
-		System.out.println("conflict resolved");
+		System.out.println("stas10");
+		System.out.println("new change");
 		
 	}
 		
