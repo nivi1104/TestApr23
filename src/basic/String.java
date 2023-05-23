e19c2c2c25b988352511ee6b5db2c8e5210781a5
Nivedita Gavade <nivedita.gavade19@gmail.com>
2023-05-23 08:40:17 +0530

    new changes

--- src/basic/String.java
+++ src/basic/String.java
@@ -2,10 +2,13 @@ package basic;
 
 public class String {
 	public static void main(String[] args) {
-		System.out.println("bbbb MAIN BRANCH OF A1");
+		System.out.println("snnnnnnnnnn");;
 		System.out.println("stash5");
-		System.out.println("creating conflict");
+		System.out.println("conflict resolved");
 		
 	}
+		
+}	
 
-}
+
+	
